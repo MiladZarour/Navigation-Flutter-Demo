d535002b6cae22b6f2af09e3f3311fc38eb196c3
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2023-01-06 16:12:27 +0100

    updating the file with write content screen 2

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -14,7 +14,7 @@ class Screen2 extends StatelessWidget {
             foregroundColor: MaterialStateProperty.all(Colors.white),
             backgroundColor: MaterialStateProperty.all(Colors.blue),
           ),
-          child: Text('Go To Screen 1'),
+          child: Text('Go Back To Screen 1'),
           onPressed: () {
             //Navigate to Screen 1
           },
